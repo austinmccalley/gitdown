93b701bb854854dc2f454067d3424247a0fc507e
Austin McCalley <austin@austinmccalley.com>
2020-09-18 16:26:28 -0700

    Output markdown file based off of commit message and specify output file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
+import sys
 
 import utils
 
 if __name__ == "__main__":
-    print("gitdown")
+    print("gitdown v0.1.0 by Austin McCalley")
+
+    output = sys.argv[1:][0] if sys.argv[1:] else 'output.md'
 
     # Check if directory is a git repository
     if not utils.checkGitFile():
@@ -12,22 +15,42 @@ if __name__ == "__main__":
 
     logs = utils.getGitLog()
 
+    # Split by commit
     logs = logs.split('\ncommit')
 
+    # Check if there are commits
     if(len(logs) < 1 or logs[0] == ''):
         print('There are no commits to be found')
         exit(1)
 
+    # Remove the commit message from the first one
     logs[0] = logs[0][7:]
 
+    commits = []
+    url = utils.getRemoteURL()
+    # Create the format string for the output
     for log in logs:
         if(log[0] == ' '):
             log = log[1:]
 
         lines = log.split('\n')
 
-        commitHash = lines[0][:6]
+        commitHashShort = lines[0][:6]
+        commitHashLong = lines[0]
         commitMessage = lines[-2][4:]
-        
-        print(commitHash)
-        print(commitMessage)
+
+        commits.append('[{}]({}) - {}'.format(commitHashShort,
+                                              url + commitHashLong, commitMessage))
+    commits.reverse()
+
+    print('Found {} commits'.format(len(commits)))
+
+    with open(output, 'w') as f:
+        f.write('# Git Logs\n\n')
+        for commit in commits:
+            f.write(commit)
+            f.write('\n\n')
+        f.write(
+            '*Powered by [gitdown](https://github.com/austinmccalley/gitdown)*')
+        print('Wrote to {}'.format(output))
+        f.close()
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,27 @@
 import os
 
+
 def checkGitFile():
     return os.path.isdir('.git')
 
 
 def getGitLog():
-  return os.popen('git log --decorate=short').read()
+    return os.popen('git log --decorate=short').read()
+
+
+def getRemoteURL():
+    origin_url = os.popen('git config --get remote.origin.url').read()
+
+    url = ''
+
+    # SSH remote url
+    if(origin_url.startswith('git@')):
+        splits = origin_url.split(':')
+
+        post = splits[-1]
+        post = post.rstrip()[:-4]
+
+        pre = splits[0].split('@')[-1]
+        return ('https://{}/{}/commit/'.format(pre, post))
+    else:
+      return origin_url[:-5]  + '/commit/'
